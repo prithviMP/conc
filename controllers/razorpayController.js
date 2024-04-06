f8a5335158c5d887282728e2bfb1fe9e0bdf6183
prithviraj m pillai <prithvi_mp@yahoo.in>
2024-04-06 22:13:45 +0530

    added  razory pay create order

--- controllers/razorpayController.js
+++ controllers/razorpayController.js
@@ -1,10 +1,30 @@
 const axiosZoho = require('../config/axiosRazorpay');
+const Razorpay = require('razorpay');
 
-exports.getLeads = async (req, res) => {
-  try {
-    const response = await axiosZoho.get('/crm/v2/Leads');
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).send(error.toString());
-  }
-};
+
+
+// Initialize Razorpay instance
+const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+  });
+  
+  exports.createOrder = async (req, res) => {
+    const { amount, currency = 'INR', receipt, notes } = req.body;
+  
+    try {
+      const options = {
+        amount: 499, // Razorpay expects the amount in the smallest currency unit (e.g., paise for INR)
+        currency,
+        receipt,
+        notes,
+      };
+  
+      const order = await razorpay.orders.create(options);
+  
+      res.json(order);
+    } catch (error) {
+      console.error('Error creating Razorpay order:', error);
+      res.status(500).send('Unable to create order');
+    }
+  };
--- routes/razorPayRoutes.js
+++ routes/razorPayRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { getLeads } = require('../controllers/razorpayController');
+const { getLeads ,createOrder} = require('../controllers/razorpayController');
 const router = express.Router();
 
-router.get('/leads', getLeads);
+//router.get('/leads', getLeads);
+router.get('/create-order', createOrder);
+
 
 module.exports = router;
