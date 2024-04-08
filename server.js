0d1c256490a5db939bb4cc133383ecd1adf69c2d
prithviraj m pillai <prithvi_mp@yahoo.in>
2024-04-08 17:23:26 +0530

    adding zoho razory webhook

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const fileRoutes = require('./routes/fileRoutes');
 const passport = require('./config/passportConfig');
 const path = require('path');
 const cors = require('cors');
+const axiosZoho = require('./config/axiosZoho');
 
 
 
@@ -68,36 +69,32 @@ app.post('/create-order', (req, res) => {
   });
 });
 
-app.post('/razorpay-webhook', (req, res) => {
+ app.post('/razorpay-webhook', async (req, res) => {
     const payment = req.body;
     const { id, name, email } = payment; // Extract relevant data based on Razorpay's payload structure
 
-    console.log("hello webhook");
+    console.log("hello webhook", payment);
     // Store in SQLite Database
-    db.run(`INSERT INTO users (id, name, email) VALUES (?, ?, ?)`, [id, name, email], (err) => {
-        if (err) return console.error(err.message);
+    // db.run(`INSERT INTO users (id, name, email) VALUES (?, ?, ?)`, [id, name, email], (err) => {
+    //     if (err) return console.error(err.message);
+      
+  
+    // });
 
-        // Call Zoho CRM API to store user information
-        const options = {
-            method: 'POST',
-            url: 'https://www.zohoapis.com/crm/v2/Leads',
-            headers: {
-                Authorization: 'Zoho-oauthtoken YOUR_ZOHO_ACCESS_TOKEN'
-            },
-            body: JSON.stringify({
-                data: [{
-                    Last_Name: name, // Assuming 'name' represents the last name
-                    Email: email,
-                    Lead_Source: 'Website'
-                }]
-            })
-        };
+    const leadData = req.body; // Extract lead data from the request
+    const data = {
+        "data": [leadData]
+      };
+      
+    console.log(data);
+    // Insert code to save leadData in your database here
 
-        request(options, (error, response) => {
-            if (error) throw new Error(error);
-            console.log(response.body);
-        });
-    });
+    try {
+        const zohoResponse = await axiosZoho.post('/crm/v6/Leads', data);
+        res.json({ success: true, data: zohoResponse.data });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
 
     res.status(200).send('Webhook received and processed');
 });
